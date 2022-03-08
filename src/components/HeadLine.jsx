5ea8272cfc6556749f8ccb6a57a5537052758ce6
eliyahu <eliyahu299@gmail.com>
2022-03-08 23:18:53 +0200

    created navbar

--- src/components/HeadLine.jsx
+++ src/components/HeadLine.jsx
@@ -1,12 +1,78 @@
 import React from 'react'
 import"../styles/HeadLine.css";
 import webSiteLogo from "../images/logo.png";
+import {NavLink} from 'react-router-dom'
+import {GoHome } from   "react-icons/go";
+import {BsQuestionLg } from   "react-icons/bs";
+import {FaFileContract } from   "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+
 
 export default function HeadLine() {
+ const navigate=useNavigate()
+/**
+ * for the stylyd links
+ */  
+const InfoNavLink=({children,to,data})=>{
+return (
+    <NavLink className={'hover:text-orange-900  items-center flex space-x-1'} to={to}>
+        {children}
+        {data}
+    </NavLink>
+)
+}
+/**
+ * log out componet
+ */
+const LogOut=()=>{
+  const logOutHandler=(e)=>{
+    localStorage.clear();
+    navigate('/login')
+  }
+  return(
+    <button className='font-bold ' onClick={logOutHandler}>
+    {'LogOut'}
+    </button>
+  )
+}
+
+const LogNavLink=({children,to})=>{
+  return(
+      <NavLink  className={ 'hover:text-green-700 text-green-500'} to={to}>
+       {children}
+      </NavLink>
+  )
+}
+
+
   return (
-    
-    <div className={"headline"}>
-        <img src={webSiteLogo} className={"WebLogo"} alt={"logo"} /> 
+     <nav className={"  bg-sky-200  bg-opacity-90 "}>
+       <div className={' mx-auto   px-16 items-center font-bold fontsi text-3xl flex justify-between'}>
+      <div className='space-x-4 flex items-center'>  { /*general conteiner */ }
+      <img src={webSiteLogo} className={"flex max-h-24"} alt={"logo"} />
+      <div className='flex space-x-4  text-amber-600 '>
+       <InfoNavLink  to={'/'} data={'Home'}>
+       <GoHome/>
+      </InfoNavLink>
+       <InfoNavLink  to={'/about'} data={'About'}>
+       <BsQuestionLg/>
+      </InfoNavLink>
+      <InfoNavLink  to={'/terms'} data={'Terms'}>
+       <FaFileContract/>
+      </InfoNavLink>
+      </div>
+      </div>
+     <div className='flex  space-x-4 items-center '> { /*the login and signin div */ }
+       <LogNavLink  to={'/Login'}>
+       {'login'}
+       </LogNavLink>
+       <LogNavLink  className={''} to={'/Register'}>
+       {'signIn'}
+      </LogNavLink>
+      <LogOut />
+     </div>
     </div>
+  </nav>
   )
 }
