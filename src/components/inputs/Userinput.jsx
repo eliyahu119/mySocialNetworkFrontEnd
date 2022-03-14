3da5d5260854ba3af5690d9a7a509547f17b9f35
eliyahu <eliyahu299@gmail.com>
2022-03-14 03:01:04 +0200

    created styled userInput

---
+++ src/components/inputs/Userinput.jsx
@@ -0,0 +1,8 @@
+import {FaRegUser} from 'react-icons/fa';
+
+ export function UserInput({handleUserChange}) {
+    return (<div className="flex items-center space-x-1 ">
+          <FaRegUser size={25} />
+          <input className="border-orange-300  opacity-90 border-2 p-1  rounded-xl" onChange={handleUserChange} required type="user" />
+         </div>);
+        }
