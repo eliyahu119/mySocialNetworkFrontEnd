fc3b5c70e9c1bda7c2093133a33e112c891d7087
eliyahu <eliyahu299@gmail.com>
2022-03-14 03:10:51 +0200

    added styled submit button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,14 @@
-
+import { StyledSubmit } from './inputs/StyledSubmit';
+import { PasswordInput } from './inputs/Passwordinput';
+import { UserInput } from './inputs/Userinput';
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import useIsUserAuth from "../hooks/useIsUserAuth";
-import { useAlert } from 'react-alert'
+import { useAlert } from 'react-alert';
+
+
+
 export default function Login() {
     const navigate = useNavigate();
     const [user, setUser] = useState('')
@@ -43,11 +48,15 @@ export default function Login() {
     }
     return (
 
-          <form className='LoginFormClass' onSubmit={handleLogin} >
-            <input className="userClass" onChange={handleUserChange} required type="user" />
-            <input className="passwordClass" onChange={handlePasswordChange} required type="password" />
-            <input  className='SubmitClass' type = "submit" value = "Submit" />
+          <form className= 'border-2  shadow-lg  flex  flex-col justify-between  lg:w-3/6  w-11/12   h-96 m-auto' onSubmit={handleLogin} >
+            <div className="  space-y-6 space-x-0  mx-auto py-12 px-1 flex flex-col lg:space-x-4 lg:space-y-0 lg:flex-row">
+            <UserInput handleUserChange={handleUserChange}  />
+            <PasswordInput   handlePasswordChange={handlePasswordChange}  />
+           </div>
+           <StyledSubmit     />
             {/* <button className="submitClass" onClick={handleLogin} >{"בראש ניתן"}</button> */}
         </form>
     )
-} 
+
+        
+  } 
