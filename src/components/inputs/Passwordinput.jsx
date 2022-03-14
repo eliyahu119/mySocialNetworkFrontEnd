902ed339dd3cd86fa3a7f26c266ba3fdeb70008d
eliyahu <eliyahu299@gmail.com>
2022-03-14 03:00:54 +0200

    created styled Passwordinput

---
+++ src/components/inputs/Passwordinput.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {MdPassword} from 'react-icons/md';
+
+
+export function PasswordInput({
+  handlePasswordChange
+}) {
+  return <div className="flex items-center space-x-1">
+           <MdPassword size={25} />
+            <input className="border-orange-300  opacity-90 border-2 p-1  rounded-xl " onChange={handlePasswordChange} required type="password" />
+           </div>;
+}
+  
